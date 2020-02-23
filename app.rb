1d6245d92384a6af57478cb0dc727532b944a4b6
GregMartinKellogg <greg.martin@kellogg.northwestern.edu>
2020-02-23 13:55:55 +0000

    Sunday Morning Update

--- app.rb
+++ app.rb
@@ -14,16 +14,6 @@ news = HTTParty.get(url).parsed_response.to_hash
 # news is now a Hash you can pretty print (pp) and parse for your output
 #pp news
 
-@headline_1 = news["articles"][2]["title"]
-@headline_1_url = news["articles"][2]["url"]
-
-# puts headline_1
-# puts headline_1_url
-
-    #"The current temperature is #{current_temp} and conditions are #{current_conditions}"
-    # </div>
-    
-
 get "/" do
   # show a view that asks for the location
   view "ask"
@@ -35,57 +25,57 @@ get "/news" do
     results = Geocoder.search(params["input"])
 
     @lat_long = results.first.coordinates # [lat, long] array 
-    #"#{lat_long[0]} #{lat_long[1]}" #writes the lat and long points to the screen
-    #"#{lat_long}"
+
     @lat = @lat_long[0]
     @long = @lat_long[1]
-    #city = results[,display_name]   
+  
     @forecast = ForecastIO.forecast(@lat,@long).to_hash
-        
+    
+    #current temps and conditions
     @current_temp= @forecast ["currently"]["temperature"] #forecast is the first hash, access the hash within using [] and calling out variable you want in ""
     @current_conditions = @forecast["currently"] ["summary"]
 
-    @headline_1 = news["articles"][2]["title"]
-    @headline_1_url = news["articles"][2]["url"]
+    #temperatur forecasts for the next 3 days
+    @forecast_temp1 = @forecast["daily"]["data"][1]["temperatureHigh"]
+    @forecast_temp2 = @forecast["daily"]["data"][2]["temperatureHigh"]
+    @forecast_temp3 = @forecast["daily"]["data"][3]["temperatureHigh"]
+
+    #tomorrow's weather summary
+    @forecast_conditions1 = @forecast["daily"]["data"][1]["summary"]
+
+    @headline_1 = news["articles"][0]["title"]
+    @headline_1_url = news["articles"][0]["url"]
+    @headline_1_photo = news["articles"][0]["urlToImage"]
+
+
+    @headline_2 = news["articles"][1]["title"]
+    @headline_2_url = news["articles"][1]["url"]
+
+    @headline_3 = news["articles"][2]["title"]
+    @headline_3_url = news["articles"][2]["url"]
+
+    @headline_3 = news["articles"][3]["title"]
+    @headline_3_url = news["articles"][3]["url"]
+
+    @headline_4 = news["articles"][4]["title"]
+    @headline_4_url = news["articles"][4]["url"]
 
     # @daily_temp = Array.new
     # @daily_summ = Array.new
 
     # i=0
-    # for daily in @forecast["daily"]["data"] do  #creates an array call daily
-    #     @daily_temp[i] = @forecast["daily"]["temperaturHigh"]
+    # for daily in @forecast["daily"]["data"]  #creates an array call daily
+    #     @daily_temp[i] = @forecast["daily"]["data"][i+1]["temperatureHigh"]
     #     @daily_summ[i] = daily["summary"]
     #     i = i + 1
     # end
 
+    # puts "daily temps are #{@daily_temp}"
 
     # for day in @forecast["daily"]["data"]
     # puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
     # end
 
     view "news"
-    #"The current temperature is #{current_temp} and conditions are #{current_conditions}"
-    # # </div>
-    
-    # </div>
-    #"In Evanston, it is currently #{current_temp} and #{current_conditions}"
-    #   </div>
-    #     # do the heavy lifting, use Global Hub lat/long
-    #     forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
-
-    #     # pp = pretty print
-    #     # use instead of `puts` to make reading a hash a lot easier
-    #     # e.g. `pp forecast`
-
-    #     current_temp= forecast ["currently"]["temperature"] #forecast is the first hash, access the hash within using [] and calling out variable you want in ""
-    #     current_conditions = forecast["currently"] ["summary"]
-
-    #     puts "In Evanston, it is currently #{current_temp} and #{current_conditions}"
-    #     #high_temp = forecast["daily"]["data"][0]["temperatureHigh"]
-    #     #puts high_temp
-
-    #     for daily_forecast in forecast["daily"]["data"] #creates an array call daily_forecast
-    #     puts "A high temperature of #{daily_forecast["temperatureHigh"]} and #{daily_forecast["summary"]}."
-    #     end
-    # </div>
+   
 end
